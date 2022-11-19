57c969f287f71369ed195518b7e88db0c99b573c
Shrine <nischalthapa10@hotmail.com>
2022-11-19 15:16:52 +0545

    Todoapi

---
+++ Models/TodoItem.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+    public class TodoItem
+    {
+        public int ID{set; get;}
+        public string Name{set;get;}
+        public string ContactNo{set;get;}
+    }
