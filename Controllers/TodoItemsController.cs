83de321e13b10bb4e4410674d5a4569b67b63ca5
Shrine <nischalthapa10@hotmail.com>
2022-12-21 09:45:03 +0545

    MYSQL connetion succesfull

--- Controllers/TodoItemsController.cs
+++ Controllers/TodoItemsController.cs
@@ -6,103 +6,150 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TodoApi.Models;
-
+using MySql.Data.MySqlClient;
 namespace TodoApi.Controllers
 {
-    [Route("api/Todoitems")]
+   
+    public class DatabaseConnection
+        {
+            private readonly string _connectionString;
+            private MySqlConnection _connection;
+
+            public DatabaseConnection(string connectionString)
+            {
+                _connectionString = connectionString;
+            }
+
+            public void Connect()
+            {
+                _connection = new MySqlConnection(_connectionString);
+                _connection.Open();
+            }
+
+            public void Disconnect()
+            {
+                _connection.Close();
+            }
+
+            public void ExecuteSql(string sql)
+            {
+                var command = new MySqlCommand(sql, _connection);
+                command.ExecuteNonQuery();
+            }
+        }
+[Route("api/Todoitems")]
     [ApiController]
     public class TodoItemsController : ControllerBase
     {
-        private readonly TodoContext _context;
+        /**() private readonly TodoContext _context;
 
-        public TodoItemsController(TodoContext context)
+         public TodoItemsController(TodoContext context)
+         {
+             _context = context;
+         }
+
+         // GET: api/TodoItems
+         [HttpGet]
+         public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
+         {
+             return await _context.TodoItems.ToListAsync();
+         }
+
+         // GET: api/TodoItems/5
+         [HttpGet("{id}")]
+         public async Task<ActionResult<TodoItem>> GetTodoItem(int id)
+         {
+             var todoItem = await _context.TodoItems.FindAsync(id);
+
+             if (todoItem == null)
+             {
+                 return NotFound();
+             }
+
+             return todoItem;
+         }
+
+         // PUT: api/TodoItems/5
+         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+         [HttpPut("{id}")]
+         public async Task<IActionResult> PutTodoItem(int id, TodoItem todoItem)
+         {
+             if (id != todoItem.Id)
+             {
+                 return BadRequest();
+             }
+
+             _context.Entry(todoItem).State = EntityState.Modified;
+
+             try
+             {
+                 await _context.SaveChangesAsync();
+             }
+             catch (DbUpdateConcurrencyException)
+             {
+                 if (!TodoItemExists(id))
+                 {
+                     return NotFound();
+                 }
+                 else
+                 {
+                     throw;
+                 }
+             }
+
+             return NoContent();
+         }
+
+         // POST: api/TodoItems
+         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+         [HttpPost]
+         public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem todoItem)
+         {
+             _context.TodoItems.Add(todoItem);
+             await _context.SaveChangesAsync();
+
+            // return CreatedAtAction("GetTodoItem", new { id = todoItem.ID }, todoItem);
+            return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, todoItem);
+         }
+
+         // DELETE: api/TodoItems/5
+         [HttpDelete("{id}")]
+         public async Task<IActionResult> DeleteTodoItem(int id)
+         {
+             var todoItem = await _context.TodoItems.FindAsync(id);
+             if (todoItem == null)
+             {
+                 return NotFound();
+             }
+
+             _context.TodoItems.Remove(todoItem);
+             await _context.SaveChangesAsync();
+
+             return NoContent();
+         }
+
+         private bool TodoItemExists(int id)
+         {
+             return _context.TodoItems.Any(e => e.Id == id);
+         }**/
+
+                private readonly DatabaseConnection _databaseConnection;
+
+        public  TodoItemsController()
         {
-            _context = context;
+            _databaseConnection = new DatabaseConnection("Server=127.0.0.1;Database=ApexRestaurantDB;Uid=root;Pwd=forlorn123X;");
+            _databaseConnection.Connect();
+
         }
 
-        // GET: api/TodoItems
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
+        public string GetData()
         {
-            return await _context.TodoItems.ToListAsync();
-        }
+            // Execute a SQL query
+            _databaseConnection.ExecuteSql("SELECT * FROM Customers");
 
-        // GET: api/TodoItems/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<TodoItem>> GetTodoItem(int id)
-        {
-            var todoItem = await _context.TodoItems.FindAsync(id);
-
-            if (todoItem == null)
-            {
-                return NotFound();
-            }
-
-            return todoItem;
-        }
-
-        // PUT: api/TodoItems/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutTodoItem(int id, TodoItem todoItem)
-        {
-            if (id != todoItem.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(todoItem).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!TodoItemExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/TodoItems
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem todoItem)
-        {
-            _context.TodoItems.Add(todoItem);
-            await _context.SaveChangesAsync();
-
-           // return CreatedAtAction("GetTodoItem", new { id = todoItem.ID }, todoItem);
-           return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, todoItem);
-        }
-
-        // DELETE: api/TodoItems/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteTodoItem(int id)
-        {
-            var todoItem = await _context.TodoItems.FindAsync(id);
-            if (todoItem == null)
-            {
-                return NotFound();
-            }
-
-            _context.TodoItems.Remove(todoItem);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        private bool TodoItemExists(int id)
-        {
-            return _context.TodoItems.Any(e => e.Id == id);
+            // Return some data
+            return "Data retrieved from the database";
         }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -19,23 +19,23 @@ public class Startup
 
     public void ConfigureServices(IServiceCollection services)
     {
-         services.AddDbContext<TodoContext>(opt =>
+        services.AddDbContext<TodoContext>(opt =>
                opt.UseInMemoryDatabase("TodoList"));
-            services.AddControllers();
+        services.AddControllers();
         services.AddRazorPages();
     }
 
-      public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
-         if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
         app.UseHttpsRedirection();
-       
+
         app.UseRouting();
         app.UseAuthorization();
-    
+
         app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
